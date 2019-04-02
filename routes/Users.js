754ca30b72a0a11fd878f82e121e21b6827db861
Your Name <you@example.com>
2019-04-02 20:45:11 +0300

    fixing code

--- routes/Users.js
+++ routes/Users.js
@@ -52,29 +52,33 @@ users.post('/register',(req,res) => {
     })
 })
 
-users.post('/loginnn',(req,res) => {
-   // res.send('Loging Route is Working')
-   if(user){
-       if (bcryptjs.compareSync(req.body.password, user.password)){
-            const payload = {
-                _id: user._id,
-                first_name: user.first_name,
-                last_name: user.last_name,
-                email: user.email
-            }     
-            let token = jwt.sign(payload, process.env.SECRET_KEY,{
-                expiresIn: 1440
-            })
-            res.send(token)
-       }else{
-           res.json({error: "User does not exist"})
-       }
-   }else{
-       res.json({error: "User does not exist"})
-   }
-})
-.catch(err => {
-    res.send("error: " + err)
+users.post('/loginnn',(req,res)=>{
+    User.findOne({
+        email: req.body.email
+    })
+    .then(user =>{
+        if(user){
+            if(bcryptjs.compareSync(rep.password, user.password)){
+                const payload = {
+                    _id: user._id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    email: user.email,
+                }
+                let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                    expiresIn: 1440
+                })
+                res.send(token)
+            }else{
+                res.json({error: "User does not exist"})
+            }
+        }else{
+            res.json({error: "User does not exist"})
+        }
+    })
+    .catch(err =>{
+        res.send('error: ' + err)
+    })
 })
 
 users.get("/profile", (req,res)=>{
@@ -85,6 +89,8 @@ users.get("/profile", (req,res)=>{
     })
     .then(user =>{
         if (user){
+            res.send(user)
+        }else{
             res.send("User does not exist")
         }
     })
