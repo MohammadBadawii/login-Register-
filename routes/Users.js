012e2075c563183aa3e08e624b820b03d13bf27f
Your Name <you@example.com>
2019-04-02 20:33:49 +0300

    react client

--- routes/Users.js
+++ routes/Users.js
@@ -53,7 +53,44 @@ users.post('/register',(req,res) => {
 })
 
 users.post('/loginnn',(req,res) => {
-    res.send('Loging Route is Working')
+   // res.send('Loging Route is Working')
+   if(user){
+       if (bcryptjs.compareSync(req.body.password, user.password)){
+            const payload = {
+                _id: user._id,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email
+            }     
+            let token = jwt.sign(payload, process.env.SECRET_KEY,{
+                expiresIn: 1440
+            })
+            res.send(token)
+       }else{
+           res.json({error: "User does not exist"})
+       }
+   }else{
+       res.json({error: "User does not exist"})
+   }
+})
+.catch(err => {
+    res.send("error: " + err)
+})
+
+users.get("/profile", (req,res)=>{
+    var decoded = jwt.verify(req.headers["authorization"], process, env.SECRET_KEY)
+
+    User.findOne({
+        _id: decoded._id
+    })
+    .then(user =>{
+        if (user){
+            res.send("User does not exist")
+        }
+    })
+    .catch(err =>{
+        res.send("error " + err)
+    })
 })
 
 module.exports = users
