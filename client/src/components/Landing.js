6d74421c9a8bb087ff900ad70269b7af5862b592
Your Name <you@example.com>
2019-04-03 19:39:21 +0300

    fixing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,15 @@
-import React , {Components} from 'react'
+import React , {Component} from 'react'
 import {Link , withRouter} from 'react-router-dom'
 
-class Landing extends Components{
+class Landing extends Component{
     render(){
         return (
             <div className="container">
-            <div className="jumbotron mt-5">
-            <div className="col-sm-8 mx-auto">
-                Welcome
-            </div>
-            </div>
+                <div className="jumbotron mt-5">
+                    <div className="col-sm-8 mx-auto">
+                        Welcome
+                    </div>
+                </div>
             </div>
         )
     }
